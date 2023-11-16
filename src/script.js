7c109dd3ad4ea920f9aca005e99643f9a8a91acc
Audrey Huvaya <135869085+Tustirai@users.noreply.github.com>
2023-11-16 09:48:30 +0200

    Revert "Merge branch 'main' of https://github.com/Tustirai/affirm-word-search"
    This reverts commit 5984230b979242036dac3cbdfe185a722585ea9c, reversing
    changes made to 533396f37169a36dc20bde9f1db2cb72175c2b88.

--- src/script.js
+++ src/script.js
@@ -291,6 +291,16 @@ function done() {
 	}
 }
 
+function resetGame() {
+	const puzzle = document.querySelector("#puzzleArea");
+	const hint = document.querySelector("#hint");
+	puzzle.innerHTML = "";
+	hint.innerHTML = "";
+	arrangeGame();
+}
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetGame);
+
 function getAffirmation(selectedWord) {
 	const affirmations = {
 		IMPROVEMENT: [
@@ -430,36 +440,16 @@ function getAffirmation(selectedWord) {
 		],
 	};
 
-	let category = selectedWord; // Make sure selectedWord is properly defined
-	let affirmationsList = document.querySelector("#newAffirmation"); // Make sure the element with ID "newAffirmation" exists
+	let category = selectedWord;
+	let affirmationsList = document.querySelector("#newAffirmation");
 	let categoryHeading = document.createElement("h4");
 	categoryHeading.textContent = category;
-	categoryHeading.className = "affirmation-heading"; // Add class to H4 element
 	affirmationsList.appendChild(categoryHeading);
-
 	if (affirmations[category]) {
-		let newAffirmations = document.createElement("div"); // Create a new div to hold the affirmations
-		newAffirmations.className = "listOfAffirmations"; // Add the class "listOfAffirmations" to the new div
-		affirmations[category].forEach((affirmation) => {
-			let olElement = document.createElement("ol"); // Create an ordered list
-			olElement.className = "affirmation-list"; // Add class to the ordered list
-			let liElement = document.createElement("li"); // Create a list item
-			liElement.textContent = affirmation;
-			olElement.appendChild(liElement); // Append list item to the ordered list
-			newAffirmations.appendChild(olElement); // Append the ordered list to the new div
-		});
-		affirmationsList.appendChild(newAffirmations); // Append the new div to the affirmationsList
+		affirmationsList.innerHTML +=
+			"<br>" + affirmations[category].join("<br>");
+	} else {
+		affirmationsList.innerHTML = " ";
 	}
+	console.log(affirmations[category]);
 }
-
-function resetGame() {
-	const puzzle = document.querySelector("#puzzleArea");
-	const hint = document.querySelector("#hint");
-	let affirmationsList = document.querySelector("#newAffirmation");
-	puzzle.innerHTML = "";
-	hint.innerHTML = "";
-	affirmationsList.innerHTML = "";
-	arrangeGame();
-}
-const resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", resetGame);
